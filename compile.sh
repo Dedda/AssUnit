63de85378fdc09b71b214b82da3c8098ca9da4dd
Stephan Göppentin <dedda102@gmail.com>
2023-01-05 21:57:46 +0100

    better output from build script

--- compile.sh
+++ compile.sh
@@ -2,17 +2,31 @@
 set -e
 
 source_files=( assunit )
-linker_command="ar rs libassunit.a"
+archive_command="ar rs libassunit.a"
+
+echo "Compiling source files:"
 
 for source_file in "${source_files[@]}"
 do
     echo "  compiling ${source_file}.asm ..."
     nasm -felf64 "${source_file}.asm"
-    linker_command="${linker_command} ${source_file}.o"
+    archive_command="${archive_command} ${source_file}.o"
 done
 
-$($linker_command)
+echo ""
+echo "Archiving with command:"
+echo "  ${archive_command}"
+echo ""
+$($archive_command)
 
 rm *.o
 
+if [ "$(command -v tokei)" != "" ]; then
+    echo "Code stats for AssUnit library:"
+    echo ""
+    tokei .
+fi
+
+echo ""
+echo "Output files:"
 ls -lh libassunit.a
